5568c7cb3493f6f8a28fcbf8ed0fbe3473c1e96e
Arman <arman.mohiuddin79@myhunter.cuny.edu>
2025-11-25 21:19:54 -0500

    initial commit, finished TonerCard comp

---
+++ src/components/TonerCard.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import {
+   Card,
+   CardContent,
+   Typography,
+   CardActions,
+   Button,
+   FormControl,
+   InputLabel,
+   Select,
+   MenuItem,
+} from "@mui/material";
+
+export default function TonerCard({ toner }) {
+   const [qty, setQty] = useState(1);
+
+   const handleAdd = () => {
+      console.log("Add to cartL", { ...toner, qty });
+   };
+}
+
+return (
+   <Card sx={{ width: 260, boxShadow: 3, borderRadius: 2 }}>
+      <CardContent>
+         <Typography variant="h6">{toner.model}</Typography>
+         <Typography color="text.secondary">{toner.printer}</Typography>
+         <Typography variant="body2" sx={{ marginTop: 1 }}></Typography>
+
+         <FormControl fullWidth sx={{ mt: 2 }}>
+            <InputLabel id="qty-label">Qty</InputLabel>
+            <Select
+               labelId="qty-label"
+               value={qty}
+               label="Qty"
+               onChange={(e) => setQty(Number(e.target.value))}
+            >
+               {[...Array(toner.stock).keys()].map((i) => (
+                  <MenuItem key={i + 1} value={i + 1}>
+                     {i + 1}
+                  </MenuItem>
+               ))}
+            </Select>
+         </FormControl>
+      </CardContent>
+
+      <CardActions>
+         <Button
+            size="small"
+            variant="contained"
+            color="primary"
+            onClick={handleAdd}
+            disabled={toner.stock === 0}
+         >
+            {toner.stock === 0 ? "Out of Stock" : "Add to Cart"}
+         </Button>
+      </CardActions>
+   </Card>
+);
