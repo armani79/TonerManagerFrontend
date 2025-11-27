71c47ce98457e25eb3cba08944d8d4cd681a5f12
Arman <arman.mohiuddin79@myhunter.cuny.edu>
2025-11-27 17:16:52 -0500

    Temporary data

--- src/data/toners.js
+++ src/data/toners.js
@@ -1,9 +1,25 @@
 // TEMPORARY DATA FOR TONERS
 
-export const tonerData = [
-   { id: 1, model: "TN-450", stock: 15, color: "Black" },
-   { id: 2, model: "CF244A", stock: 8, color: "Black" },
-   { id: 3, model: "CE285A", stock: 0, color: "Black" },
-   { id: 4, model: "TN-230", stock: 20, color: "Cyan" },
-   { id: 5, model: "CF243A", stock: 5, color: "Cyan" },
+export const tempToners = [
+   {
+      id: 1,
+      model: "Lexmark 501H",
+      color: "Black",
+      printers: ["MS610dn", "MS510dn"],
+      stock: 4,
+   },
+   {
+      id: 2,
+      model: "HP 12A Black",
+      color: "Black",
+      printers: ["LaserJet 1010", "LaserJet 1022"],
+      stock: 1,
+   },
+   {
+      id: 3,
+      model: "Xerox 006R",
+      color: "Cyan",
+      printers: ["VersaLink C400"],
+      stock: 7,
+   },
 ];
