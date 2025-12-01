b86e14ce50f774247623f8fff02f35e05fb7d603
Arman <arman.mohiuddin79@myhunter.cuny.edu>
2025-12-01 17:41:14 -0500

    adding register route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inventory from "./pages/Inventory.jsx";
 import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 import { TonerProvider } from "./context/TonerContext";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
             <Routes>
                <Route path="/inventory" element={<Inventory />} />
                <Route path="/login" element={<Login />} />
+               <Route path="/register" element={<Register />} />
             </Routes>
          </BrowserRouter>
       </TonerProvider>
