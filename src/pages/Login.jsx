7b0c46a9ff5d94ac113e931ee4de22629f67addc
Arman <arman.mohiuddin79@myhunter.cuny.edu>
2025-12-01 17:41:40 -0500

    adding code for login page and styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,50 +21,79 @@ export default function Login() {
    }
 
    return (
-      <div style={{ maxWidth: 400, margin: "auto", padding: 20 }}>
-         <Typography variant="h4" gutterBottom>
-            Login
-         </Typography>
-         <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={LoginSchema}
-            onSubmit={fakeLogin}
-         >
-            {({ errors, touched, handleChange }) => (
-               <Form>
-                  <TextField
-                     label="Email"
-                     name="email"
-                     fullWidth
-                     margin="normal"
-                     onChange={handleChange}
-                     error={touched.email && Boolean(errors.email)}
-                     helperText={touched.email && errors.email}
-                  />
-                  <TextField
-                     label="Password"
-                     name="password"
-                     type="password"
-                     fullWidth
-                     margin="normal"
-                     onChange={handleChange}
-                     error={touched.password && Boolean(errors.password)}
-                     helperText={touched.password && errors.password}
-                  />
-                  <Button type="submit" variant="contained">
-                     Login
-                  </Button>
+      <div style={{ display: "flex", height: "100vh", width: "100vw" }}>
+         <div
+            style={{
+               flex: 1,
+               backgroundColor: 'url("/backgroundLogin.jpg")',
+               backgroundSize: "cover",
+               backgroundPosition: "center",
+               backgroundRepeat: "no-repeat",
+               display: "flex",
+               justifyContent: "center",
+               alignItems: "center",
+               padding: 20,
+               color: "white",
+            }}
+         ></div>
 
-                  <Button
-                     type="button"
-                     variant="text"
-                     onClick={() => navigate("/register")}
-                  >
-                     Need an account? Register
-                  </Button>
-               </Form>
-            )}
-         </Formik>
+         <div
+            style={{
+               flex: 1.2,
+               backgroundColor: "white",
+               display: "flex",
+               justifyContent: "flex-start",
+               alignItems: "center",
+               paddingLeft: "80px",
+               borderLeft: "1px solid #e0e0e0",
+            }}
+         >
+            <div style={{ width: "350px" }}>
+               <Typography variant="h4" gutterBottom>
+                  Login
+               </Typography>
+               <Formik
+                  initialValues={{ email: "", password: "" }}
+                  validationSchema={LoginSchema}
+                  onSubmit={fakeLogin}
+               >
+                  {({ errors, touched, handleChange }) => (
+                     <Form>
+                        <TextField
+                           label="Email"
+                           name="email"
+                           fullWidth
+                           margin="normal"
+                           onChange={handleChange}
+                           error={touched.email && Boolean(errors.email)}
+                           helperText={touched.email && errors.email}
+                        />
+                        <TextField
+                           label="Password"
+                           name="password"
+                           type="password"
+                           fullWidth
+                           margin="normal"
+                           onChange={handleChange}
+                           error={touched.password && Boolean(errors.password)}
+                           helperText={touched.password && errors.password}
+                        />
+                        <Button type="submit" variant="contained">
+                           Login
+                        </Button>
+
+                        <Button
+                           type="button"
+                           variant="text"
+                           onClick={() => navigate("/register")}
+                        >
+                           Need an account? Register
+                        </Button>
+                     </Form>
+                  )}
+               </Formik>
+            </div>
+         </div>
       </div>
    );
 }
