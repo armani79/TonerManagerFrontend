ef7c7de002c7172467735b0e3298639d82b8655d
Arman <arman.mohiuddin79@myhunter.cuny.edu>
2025-12-03 20:53:53 -0500

    universal snackbar for alerts

---
+++ src/components/AppSnackbar.jsx
@@ -0,0 +1,22 @@
+// Reusable snackbar alert
+import { Snackbar, Alert } from "@mui/material";
+
+export default function AppSnackbar({
+   open,
+   onClose,
+   message,
+   severity = "info",
+}) {
+   return (
+      <Snackbar
+         open={open}
+         autoHideDuration={3000}
+         onClose={onClose}
+         anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      >
+         <Alert onClose={onClose} severity={severity} sx={{ width: "100%" }}>
+            {message}
+         </Alert>
+      </Snackbar>
+   );
+}
