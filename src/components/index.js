da955c5d34f96b7ef383630013e75b7727303ef8
Arman <arman.mohiuddin79@myhunter.cuny.edu>
2025-12-11 19:42:36 -0500

    index files

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as TonerCard } from "./TonerCard";
+export { default as AppSnackbar } from "./AppSnackbar";
+export { default as LogoutButton } from "./LogoutButton";
+export { default as NavBar } from "./NavBar";
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Inventory } from "./Inventory";
+export { default as Checkout } from "./Checkout";
+export { default as Login } from "./Login";
+export { default as Register } from "./Register";
+export { default as TonerDetails } from "./TonerDetail";
+export { default as EditToner } from "./EditToner";
