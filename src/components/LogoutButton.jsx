9828803f9ed080044e2173a24f9e52dcb97a8fd8
Arman <arman.mohiuddin79@myhunter.cuny.edu>
2025-12-11 19:42:10 -0500

    adding logout button comp

---
+++ src/components/LogoutButton.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
+function LogoutButton() {
+   const navigate = useNavigate();
+
+   const logout = () => {
+      localStorage.removeItem("token");
+      navigate("/");
+   };
+
+   return <Button onClick={logout}>Logout</Button>;
+}
+
+export default LogoutButton;
